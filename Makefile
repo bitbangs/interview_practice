0630bf12dc700ca56fcd0700541be350db73d57a
bitbangs <ralexchristo@gmail.com>
2019-11-02 00:11:49 -0400

    merge sort fails on odd size input

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ sort: sort.o
 	g++ bin/sort.o -o bin/sort
 
 sort.o: src/sort.cpp
-	g++ -c src/sort.cpp -o bin/sort.o
+	g++ -c -g src/sort.cpp -o bin/sort.o
 
 clean:
 	rm -rf bin/sort.o bin/sort
--- src/sort.cpp
+++ src/sort.cpp
@@ -1,6 +1,7 @@
-#include <algorithm>
-#include <iostream>
-#include <random>
+#include <algorithm> //is_sorted
+#include <iostream> //test output
+#include <stack> //non-recursive merge sort
+#include <random> //vector gen
 #include <vector>
 using namespace std;
 
@@ -24,7 +25,7 @@ vector<int> CreateRandomVector() {
 	return random_vec;
 }
 
-vector<int> InsertionSort(std::vector<int> input) {
+vector<int> InsertionSort(vector<int> input) {
 	vector<int> output;
 	output.reserve(input.size());
 	for (auto ii = input.begin(); ii != input.end(); ++ii) {
@@ -35,13 +36,92 @@ vector<int> InsertionSort(std::vector<int> input) {
 			}
 		}
 	}
+	if (input.size() > output.size()) { //last element in input was already in sorted place
+		output.push_back(input[input.size() - 1]);
+	}
+	return output;
+}
+
+vector<int> Merge(vector<int> left, vector<int> right) {
+	vector<int> merged;
+	merged.reserve(left.size() + right.size());
+	auto left_ii = left.begin();
+	auto left_end = left.end();
+	bool left_done = false;
+	auto right_ii = right.begin();
+	auto right_end = right.end();
+	bool right_done = false;
+	while (left_ii != left_end) {
+		if (right_ii != right_end) {
+			if (*left_ii < *right_ii) {
+				merged.push_back(*left_ii);
+				++left_ii;
+			}
+			else {
+				merged.push_back(*right_ii);
+				++right_ii;
+			}
+		}
+		else {
+			merged.push_back(*left_ii);
+			++left_ii;
+		}
+	}
+	while (right_ii != right_end) {
+		merged.push_back(*right_ii);
+		++right_ii;
+	}
+	return merged;
+}
+
+vector<int> MergeSort(vector<int> input) {
+	vector<int> output;
+	output.reserve(input.size());
+	stack<vector<int>> merge_stack;
+	for (int ii = 0; ii < input.size(); ii += 2) {
+		vector<int> sub;
+		int left = input[ii];
+		int right = input[ii + 1];
+		//2 element insertion sort
+		if (left < right) {
+			sub.push_back(left);
+			sub.push_back(right);
+		}
+		else {
+			sub.push_back(right);
+			sub.push_back(left);
+		}
+		merge_stack.push(sub); //each sub is sorted (2 elems)
+	}
+	//merge sorted sub arrays
+	while (merge_stack.size() > 0 && merge_stack.size() % 2 == 0) {
+		auto left = merge_stack.top();
+		merge_stack.pop();
+		auto right = merge_stack.top();
+		merge_stack.pop();
+		auto merged = Merge(left, right);
+		merge_stack.push(merged);
+	}
+	output = merge_stack.top();	
+	//odd number of elements? insert last item into proper place in sorted array
+	if (input.size() % 2 > 0) {
+		int leftover = input[input.size() - 1];
+		for (auto ii = input.begin(); ii != input.end(); ++ii) {
+			if (*ii > leftover) {
+				output.insert(ii, leftover);
+			}
+		}
+	}
 	return output;
 }
 
 int main(int argc, char* argv[]) {
 	vector<int> input = CreateRandomVector();
+	cout << "size: " << input.size() << '\n';
 	vector<int> output = InsertionSort(input);
 	cout << "insertion sort: " << is_sorted(output.begin(), output.end()) << '\n';
+	output = MergeSort(input);
+	cout << "merge sort: " << is_sorted(output.begin(), output.end()) << '\n';
 
 	return 0;
 }
